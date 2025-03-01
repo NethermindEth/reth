bd0bab259df3a5ab26bd7743dc4c01294ff2b49a
Solar Mithril <solarmithril@pm.me>
2025-03-01 13:39:52 +0700

    Improve logging

--- crates/optimism/rpc/src/eth/transaction.rs
+++ crates/optimism/rpc/src/eth/transaction.rs
@@ -34,6 +34,7 @@ where
     /// Returns the hash of the transaction.
     async fn send_raw_transaction(&self, tx: Bytes) -> Result<B256, Self::Error> {
         println!("Got raw transaction");
+        panic!("send_raw_transaction");
         let recovered = recover_raw_transaction(&tx)?;
         let pool_transaction = <Self::Pool as TransactionPool>::Transaction::from_pooled(recovered);
 
